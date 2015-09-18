9d7208291f33e8d15ed896bbca1a1db8fed84c94
Danil Mikhailov <danil@altlinux.org>
2015-09-18 19:14:07 +0300

    Added population load

--- city_github.sh
+++ city_github.sh
@@ -12,6 +12,35 @@ var=0
     done
 }
 
+load_people(){
+#<td class="row1" align="right">10 381 288</td>
+
+site="http://www.tiptopglobe.com/biggest-cities-world"
+pqdata="?p"
+var=0
+    for i in {1..10} ; do
+	pqreq="${pqdata}=${var}"
+        wget -O - "$site"$pqreq \ |
+        iconv -f windows-1250 -t utf-8 biggest-cities-world \ |
+        egrep -Eo "(<font color=\"black\">[^<-]+</font>|<td class=\"row[12]\" align=\"right\">[0-9 ]+</td>)" \ |
+        paste - - \ |
+	sed "s|<font color=\"black\">||g" | sed "s|</font>||g" | sed "s|<td class=\"row[12]\" align=\"right\">||g" \ |
+	sed "s|</td>||g" | sed "s|(.*)||g"
+
+        # grep -Eo '<td class=\"row1\" align=\"right\">[0-9 ]+</td>' | sed "s|<td class=\"row1\" align=\"right\">||g" | sed "s|</td>||g" >> city_people
+	var=$((var+1))
+    done
+
+ 
+}
+
+example(){
+
+# egrep -Eo "(<font color=\"black\">[^<]+</font>|<td class=\"row[12]\" align=\"right\">[0-9 ]+</td>)" \ |
+# sed "s|<font color=\"black\">||g" | sed "s|</font>||g" | sed "s|<td class=\"row1\" align=\"right\">||g" | sed "s|</td>||g" 
+
+}
+
 #parse_city(){}
 
 load_statistic(){
@@ -28,4 +57,5 @@ load_statistic(){
 }
 
 #load_cities
-load_statistic
+load_people
+#load_statistic
