ad940bfb7d2aeb38578a7aa2322cdfeb3a0f0fc1
Danil Mikhailov <danil@altlinux.org>
2015-09-19 15:02:24 +0300

    Added separation by language, fixed city with space in name

--- city_github.sh
+++ city_github.sh
@@ -31,16 +31,31 @@ echo "City Population "  > $city_people
     done
 }
 
-load_statistic(){
-#    sleep 60
-
+load_users_per_city(){
     echo "City Users" > $city_github_user
     while read city ; do
-	echo -n "$city " >> $city_github_user
-    	curl -H 'Accept: application/vnd.github.v3.text-match+json'   "https://api.github.com/search/users?q=location:$city" \
-	 | grep "total_count" | sed "s|  \"total_count\": ||g" | sed "s|,||" >> $city_github_user
-	sleep 7
+        echo -n "$city " >> $city_github_user
+        curl -H 'Accept: application/vnd.github.v3.text-match+json'   "https://api.github.com/search/users?q=location:$city" \
+	| grep "total_count" | sed "s|  \"total_count\": ||g" | sed "s|,||" >> $city_github_user
+        sleep 7
     done < city
+}
+
+load_users_per_city_and_language(){
+    city_github_user_per_language=city_github_user_per_language
+    echo "City   Users 'C#'  Java     PHP JavaScript Objective-C Ruby Python C++ C" > $city_github_user_per_language
+    while read city ; do
+        echo -n "$city " >> $city_github_user_per_language
+	for language in '*' "C%23" Java PHP JavaScript Objective-C Ruby Python "C%2B%2B" C ; do
+            #echo -en "\t $language \t" >> $city_github_user_per_language
+            echo -en ' \t ' >> $city_github_user_per_language
+            my_city="%22$(echo $city | sed "s| |%20|g")%22"
+    	    curl -H 'Accept: application/vnd.github.v3.text-match+json'   "https://api.github.com/search/users?q=location:${my_city}+language:$language" \
+		| grep "total_count" | sed "s|  \"total_count\": ||g" | sed "s|,||" | tr -d "\n" >> $city_github_user_per_language
+	    sleep 7
+	done
+	echo " " >> $city_github_user_per_language
+    done < city_manual_correct
 }
 
 my_merge(){
@@ -49,11 +64,9 @@ my_merge(){
 	grep -m 1 "$city" $city_people | tr -d "\n"
 	p=$( grep -m 1 "$city" $city_people | grep -Eo "[0-9 ]+" | tr -d " ")
 	echo -en ' \t '
-#	echo -en ' !!! '
-	g=$(grep -m 1 "$city" $city_github_user | sed "s|"$city"||g")
+	g=$(grep -m 1 "$city" $city_github_user_per_language | sed "s|"$city"||g")
 	echo -en "$g"
 	echo -en ' \t '
-#	echo -en ' !!! '
 	gr=$(echo $g | grep -m 1 -Eo "[0-9 ]+")
 	perl -E "say (${gr}/(${p}/1000))" | cut -c1-7
     done < city
@@ -61,5 +74,6 @@ my_merge(){
 
 #load_cities
 #load_people
-#load_statistic
-my_merge
+load_users_per_city_and_language
+#load_users_per_city
+#my_merge
