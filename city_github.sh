c37a698e02bc06b8a3899f72d2dc28f5bfb4b57e
Danil Mikhailov <danil@altlinux.org>
2015-09-24 00:59:57 +0300

    Refactor added russian cities

--- city_github.sh
+++ city_github.sh
@@ -46,21 +46,30 @@ load_users_by_city(){
 	| grep "total_count" | sed "s|  \"total_count\": ||g" | sed "s|,||" >> $city_github_user
 }
 
+
+load_users_per_world_cities_and_language(){
+    load_users_per_cities_and_language "city_manual_correct"
+}
+
+load_users_for_russian_cities_and_language(){
+    load_users_per_cities_and_language "cities_of_russia"
+}
+
 load_users_per_cities_and_language(){
-    echo "City   Users 'C#'  Java     PHP JavaScript Objective-C Ruby Python C++ C" > $city_github_user_per_language
+    echo "City   Users 'C#'  Java     PHP JavaScript Objective-C Ruby Python C++ C"
     while read city ; do
 	load_users_and_languages_per_city "$city"
-    done < city_manual_correct
+    done < "$1"
 }
 
 load_users_and_languages_per_city(){
-	echo " " >> $city_github_user_per_language
-        echo -n "$1 " >> $city_github_user_per_language
+	echo " "
+        echo -n "$1 "
 	for language in '*' "C%23" Java PHP JavaScript Objective-C Ruby Python "C%2B%2B" C ; do
-            echo -en ' \t ' >> $city_github_user_per_language
+            echo -en ' \t '
             my_city="%22$(echo $1 | sed "s| |%20|g")%22"
     	    curl -H 'Accept: application/vnd.github.v3.text-match+json'   "https://api.github.com/search/users?q=location:${my_city}+language:$language" \
-		| grep "total_count" | sed "s|  \"total_count\": ||g" | sed "s|,||" | tr -d "\n" >> $city_github_user_per_language
+		| grep "total_count" | sed "s|  \"total_count\": ||g" | sed "s|,||" | tr -d "\n" 
 	    sleep 7
 	done
 }
@@ -101,9 +110,9 @@ var=1
     	#echo "<td align=\"left\">$(grep -m 1 "$city" $city_people | grep -Eo "[A-z -]+" | tr -d "\n")</td>"
 	echo "<td align=\"left\">$city</td>"
 	
-	g=$(cat city_github_user_per_language | grep -m 1 "^$city  	" | cut -f 2)
+	g=$(cat $1 | grep -m 1 "^$city  	" | cut -f 2)
 	gr=$(echo $g | grep -m 1 -Eo "[0-9 ]+")
-	p=$( grep -m 1 "^$city" $city_people | grep -Eo "[0-9 ]+" | tr -d " ")
+	p=$( grep -m 1 "^$city" $2 | grep -Eo "[0-9 ]+" | tr -d " ")
 	my_echo $(perl -E "say (${gr}/(${p}/1000))" | cut -c1-7)
 	#"
 #	my_echo "$g"
@@ -111,17 +120,20 @@ var=1
 	my_echo "$p"
 
 	echo -n "<td align=\"right\">"
-	cat city_github_user_per_language | grep -m 1 "^$city  	" | cut -f 2,3,4,5,6,7,8,9,10,11 | sed "s|\t|</td><td align=\"right\">|g"
+	cat $1 | grep -m 1 "^$city  	" | cut -f 2,3,4,5,6,7,8,9,10,11 | sed "s|\t|</td><td align=\"right\">|g"
 	echo -n "</td"
 
 	echo "</tr>"
 	var=$((var+1))
 
-    done < city_manual_correct_uniq
+    done < $3
 
 echo '</table>'
 echo '</html>'
 }
+
+#
+
 
 my_echo(){
 echo "<td align=\"right\">$1</td>"
@@ -133,6 +145,9 @@ echo "<td align=\"right\">$1</td>"
 #load_users_per_city
 #load_users_and_languages_per_city Boston
 #load_users_by_city Boston #TODO write test which compare out with Boston ~8808
-#my_merge
+#to_html_table city_github_user_per_language city_people city_manual_correct
 
-to_html_table
+#load_users_for_russian_cities_and_language >> russian_cities_github_user_per_language
+
+to_html_table russian_cities_github_user_per_language cities_of_russia_with_population cities_of_russia
+
